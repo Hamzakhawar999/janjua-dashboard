c1826d3ea1d904643887d86c06fcb7baf4d80ad4
Hamza Khawar <apple@HKs-MacBook-Pro.local>
2025-08-25 04:19:09 +0500

    Rebuild web with correct base href

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -3,7 +3,7 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "b7230b480935efb26b6a1c65a1ddefd3",
+const RESOURCES = {"flutter_bootstrap.js": "1a62f1c052668b651488dcb3e1af8630",
 "version.json": "ededb7c56bac835dceba00d055d46a0f",
 "index.html": "d794df2446cc1b36837287f8d8e89b53",
 "/": "d794df2446cc1b36837287f8d8e89b53",
