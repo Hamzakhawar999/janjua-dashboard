a9206f0d0fe8f388f88dcee60c4a2b7b90cc4fde
Hamza Khawar <apple@HKs-MacBook-Pro.local>
2025-08-25 04:16:23 +0500

    Add Flutter web build

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "754851c21112acb0c264aefe2ebb26de",
+const RESOURCES = {"flutter_bootstrap.js": "b7230b480935efb26b6a1c65a1ddefd3",
 "version.json": "ededb7c56bac835dceba00d055d46a0f",
 "index.html": "d794df2446cc1b36837287f8d8e89b53",
 "/": "d794df2446cc1b36837287f8d8e89b53",
-"main.dart.js": "6ed2baa858310148df3b361c18e59523",
+"main.dart.js": "d34c23fec23a9d0d2fe15f4067dc4e52",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
@@ -23,7 +23,7 @@ const RESOURCES = {"flutter_bootstrap.js": "754851c21112acb0c264aefe2ebb26de",
 "assets/packages/flutter_map/lib/assets/flutter_map_logo.png": "208d63cc917af9713fc9572bd5c09362",
 "assets/shaders/ink_sparkle.frag": "ecc85a2e95f5e9f53123dcaf8cb9b6ce",
 "assets/AssetManifest.bin": "b0373528929e5e4e3ffef5a228524ce3",
-"assets/fonts/MaterialIcons-Regular.otf": "3db5e4278dc50cf57e81a0e6bd803518",
+"assets/fonts/MaterialIcons-Regular.otf": "93c110658ea9a01be2f91de1775367bf",
 "assets/assets/images/greek_salad.jpeg": "d57a6cee801bb95f64cb0839ad430dd6",
 "assets/assets/images/apple_logo.png": "c9403999e859bfdc224f5bb6ab3d33d7",
 "assets/assets/images/drinks.jpeg": "66db070b7252c4fda9f3aac8ab3ca6cc",
